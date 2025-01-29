1c60053b07465a43381958b33c2b05b908e2c849
Preethi <writetopreethir@gmail.com>
2025-01-28 21:56:16 -0800

    Chatbot app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Chatbot from './Chatbot';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Chatbot></Chatbot>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Chatbot.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import { View, Text } from "react-native";
+import { GiftedChat, IMessage } from "react-native-gifted-chat";
+
+const Chatbot : React.FC = () => {
+  const [message, setMessage] = useState<IMessage>([])
+
+  return(
+   <View>
+      <Text>Chatbot</Text>
+   </View>
+  )
+}
+
+export default Chatbot;
